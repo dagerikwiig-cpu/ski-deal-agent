b8720d105f29398d66dd871874705f43653cb68d
dagerikwiig-cpu <dag.erik.wiig@gmail.com>
2026-03-06 15:45:29 +0100

    Update stores.py

--- stores.py
+++ stores.py
@@ -1,6 +1,7 @@
 STORES = [
 
 ("XXL","https://www.xxl.no/salg"),
+("XXL","https://www.xxl.no/kampanje?categoryPath=9100000"),
 ("Fjellsport","https://www.fjellsport.no/salg"),
 ("Sport1","https://www.sport1.no/salg"),
 ("Anton Sport","https://www.antonsport.no/salg"),
