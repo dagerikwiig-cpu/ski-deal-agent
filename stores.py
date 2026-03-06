28f8bac807e8267ac7e2fe477ca7ac8cf65d0229
dagerikwiig-cpu <dag.erik.wiig@gmail.com>
2026-03-06 14:31:49 +0100

    Create stores.py

---
+++ stores.py
@@ -0,0 +1,9 @@
+STORES = [
+
+("XXL","https://www.xxl.no/salg"),
+("Fjellsport","https://www.fjellsport.no/salg"),
+("Sport1","https://www.sport1.no/salg"),
+("Anton Sport","https://www.antonsport.no/salg"),
+("Outnorth","https://www.outnorth.no/sale")
+
+]
